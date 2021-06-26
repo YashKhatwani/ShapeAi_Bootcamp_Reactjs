e61ecc3a79221b47c697f72895315e9874a7244d
Yash Khatwani <67253796+YashKhatwani@users.noreply.github.com>
2021-06-26 20:27:16 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and reactjs</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni
+        pariatur, nesciunt doloribus ab unde provident sed ipsum sapiente, nisi
+        aliquam similique. Qui fugiat sint quas consequatur reprehenderit
+        nostrum eligendi excepturi.
+      </p>
+    </div>
+  );
+}
+export default Info;
